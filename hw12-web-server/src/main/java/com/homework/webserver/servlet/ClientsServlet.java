17352fe5047833a4cb430a36e5d2c705776d22a8
Artem Beresnev <artembert@gmail.com>
2025-02-15 22:46:30 +0100

    HW-12: display clients from Servlet's data

--- hw12-web-server/src/main/java/com/homework/webserver/servlet/ClientsServlet.java
+++ hw12-web-server/src/main/java/com/homework/webserver/servlet/ClientsServlet.java
@@ -1,6 +1,9 @@
 package com.homework.webserver.servlet;
 
 import com.google.gson.Gson;
+import com.homework.webserver.crm.model.Address;
+import com.homework.webserver.crm.model.Client;
+import com.homework.webserver.crm.model.Phone;
 import com.homework.webserver.dao.UserDao;
 import com.homework.webserver.services.TemplateProcessor;
 import jakarta.servlet.http.HttpServlet;
@@ -8,6 +11,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SuppressWarnings({"java:S1989"})
 public class ClientsServlet extends HttpServlet {
@@ -15,6 +21,7 @@ public class ClientsServlet extends HttpServlet {
     private static final String PARAM_NAME = "name";
     private static final String PARAM_ADDRESS = "address";
     private static final String PARAM_PHONE = "phone";
+    private static final Logger log = LoggerFactory.getLogger(ClientsServlet.class);
 
     private final transient UserDao userDao;
     private final transient Gson gson;
@@ -29,7 +36,7 @@ public class ClientsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-        response.getWriter().println(templateProcessor.getPage(PAGE_TEMPLATE, new HashMap<>()));
+        response.getWriter().println(templateProcessor.getPage(PAGE_TEMPLATE, getParamsMap()));
     }
 
     @Override
@@ -40,7 +47,14 @@ public class ClientsServlet extends HttpServlet {
         System.out.println(name + " " + address + " " + phone);
 
         response.setContentType("text/html");
-        response.getWriter().println(templateProcessor.getPage(PAGE_TEMPLATE, new HashMap<>()));
+        response.getWriter().println(templateProcessor.getPage(PAGE_TEMPLATE, getParamsMap()));
+    }
 
+    private HashMap<String, Object> getParamsMap() {
+        HashMap<String, Object> paramsMap = new HashMap<>();
+        paramsMap.put("clients", List.of(new Client(1L, "John Show", new Address(1L, "Winterfell, The North, Westeros"),
+            List.of(new Phone(1L, "+123 456 78 90"), new Phone(1L, "+413 419 923 12 42")))
+        ));
+        return paramsMap;
     }
 }
