7468bca55adbb4b6b80041ebc5d1bf4f91107896
Artem Beresnev <artembert@gmail.com>
2025-02-16 12:27:32 +0100

    HW-12: delete irrelevant tests

--- hw12-web-server/src/test/java/com/homework/webserver/UsersWebServerImplTest.java
+++ hw12-web-server/src/test/java/com/homework/webserver/UsersWebServerImplTest.java
@@ -4,11 +4,9 @@ import static com.homework.webserver.utils.WebServerHelper.buildUrl;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
-import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.homework.webserver.crm.service.DBServiceAccount;
 import com.homework.webserver.crm.service.DBServiceClient;
-import com.homework.webserver.model.User;
 import com.homework.webserver.server.UsersWebServer;
 import com.homework.webserver.server.UsersWebServerSimple;
 import com.homework.webserver.services.TemplateProcessor;
@@ -17,6 +15,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -27,13 +26,8 @@ class UsersWebServerImplTest {
 
     private static final int WEB_SERVER_PORT = 8989;
     private static final String WEB_SERVER_URL = "http://localhost:" + WEB_SERVER_PORT + "/";
-    private static final String API_USER_URL = "api/user";
+    private static final String CLIENTS_URL = "clients";
 
-    private static final long DEFAULT_USER_ID = 1L;
-
-    private static final User DEFAULT_USER = new User(DEFAULT_USER_ID, "Vasya", "user1", "11111");
-
-    private static Gson gson;
     private static UsersWebServer webServer;
     private static HttpClient http;
 
@@ -45,7 +39,6 @@ class UsersWebServerImplTest {
         DBServiceClient dbServiceClient = mock(DBServiceClient.class);
         DBServiceAccount dbServiceAccount = mock(DBServiceAccount.class);
 
-        gson = new GsonBuilder().serializeNulls().create();
         webServer = new UsersWebServerSimple(WEB_SERVER_PORT, templateProcessor, dbServiceClient, dbServiceAccount);
         webServer.start();
     }
@@ -59,12 +52,12 @@ class UsersWebServerImplTest {
     @Test
     void shouldReturnCorrectUserWhenAuthorized() throws Exception {
         HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create(buildUrl(WEB_SERVER_URL, API_USER_URL, String.valueOf(DEFAULT_USER_ID))))
-                .build();
+            .GET()
+            .uri(URI.create(buildUrl(WEB_SERVER_URL, CLIENTS_URL)))
+            .build();
         HttpResponse<String> response = http.send(request, HttpResponse.BodyHandlers.ofString());
 
         assertThat(response.statusCode()).isEqualTo(HttpURLConnection.HTTP_OK);
-        assertThat(response.body()).isEqualTo(gson.toJson(DEFAULT_USER));
+        assertThat(response.headers().map().get("Content-Type")).isEqualTo(List.of("text/html;charset=utf-8"));
     }
 }
