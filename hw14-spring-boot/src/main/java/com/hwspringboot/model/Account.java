b055048f19222b404846db8161742d1c05320ec4
Artem Beresnev <artembert@gmail.com>
2025-06-10 13:03:52 +0300

    HW-14: Copy models from hw12

---
+++ hw14-spring-boot/src/main/java/com/hwspringboot/model/Account.java
@@ -0,0 +1,30 @@
+package com.hwspringboot.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "account")
+public class Account {
+    @Id
+    @SequenceGenerator(name = "account_gen", sequenceName = "account_seq", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "account_gen")
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String email;
+    private String password;
+}
