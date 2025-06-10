e4bc61657eeae24854966fc7101989dfb7a42091
Artem Beresnev <artembert@gmail.com>
2025-06-10 18:07:53 +0300

    HW-14: Split ClientRest controller and ClientUI controller

--- hw14-spring-boot/src/main/java/com/hwspringboot/controllers/ClientRestController.java
+++ hw14-spring-boot/src/main/java/com/hwspringboot/controllers/ClientRestController.java
@@ -6,9 +6,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/clients")
 public class ClientRestController {
 
     private final ClientService clientService;
@@ -17,7 +19,7 @@ public class ClientRestController {
         this.clientService = clientService;
     }
 
-    @GetMapping({"/", "/clients"})
+    @GetMapping(value = "/", produces = "application/json")
     public String clientsList() {
         List<Client> clients = clientService.findAll();
         return clients.stream().map(Client::toString).collect(Collectors.joining(", "));
