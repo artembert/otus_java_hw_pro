1e4a190940b2bfab59d53aa168632246f098203f
Artem Beresnev <artembert@gmail.com>
2025-06-10 13:01:47 +0300

    HW-14: Rename root package name from hwspringboot.hwspringboot to com.hwspringboot

--- hw14-spring-boot/src/main/java/com/hwspringboot/Hw14SpringBootApplication.java
+++ hw14-spring-boot/src/main/java/com/hwspringboot/Hw14SpringBootApplication.java
@@ -1,4 +1,4 @@
-package hwspringboot.hwspringboot;
+package com.hwspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- hw14-spring-boot/src/test/java/com/hwspringboot/Hw14SpringBootApplicationTests.java
+++ hw14-spring-boot/src/test/java/com/hwspringboot/Hw14SpringBootApplicationTests.java
@@ -1,4 +1,4 @@
-package hwspringboot.hwspringboot;
+package com.hwspringboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
