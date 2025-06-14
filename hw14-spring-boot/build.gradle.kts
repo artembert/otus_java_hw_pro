bdf9d335ebaff427771e9e7dbab8811045d5943e
Artem Beresnev <artembert@gmail.com>
2025-06-14 13:17:59 +0300

    HW-14: remove unused test

--- hw14-spring-boot/build.gradle.kts
+++ hw14-spring-boot/build.gradle.kts
@@ -4,9 +4,6 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "hw-spring-boot"
-version = "0.0.1-SNAPSHOT"
-
 java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(21)
@@ -39,7 +36,3 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
-
-tasks.named<Test>("test") {
-	useJUnitPlatform()
-} 
