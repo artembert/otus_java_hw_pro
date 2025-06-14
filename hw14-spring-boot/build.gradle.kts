5142134485983522fab9d7f3949aeb5a8ffe667f
Artem Beresnev <artembert@gmail.com>
2025-06-14 13:38:24 +0300

    HW-14: inherit hw14 from the root project

--- hw14-spring-boot/build.gradle.kts
+++ hw14-spring-boot/build.gradle.kts
@@ -1,13 +1,7 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.5.0"
-	id("io.spring.dependency-management") version "1.1.7"
-}
-
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+	id("org.springframework.boot")
+	id("io.spring.dependency-management")
 }
 
 configurations {
