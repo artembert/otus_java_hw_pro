c972934915da35f77fa6aa0d18ce9e49aafe02df
Artem Beresnev <artembert@gmail.com>
2025-06-14 14:37:51 +0300

    HW-14: split UI and REST controllers

--- hw14-spring-boot/src/main/java/com/hwspringboot/controllers/ClientRestController.java
+++ hw14-spring-boot/src/main/java/com/hwspringboot/controllers/ClientRestController.java
@@ -7,12 +7,12 @@ import com.hwspringboot.services.ClientService;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequestMapping("/api/v1/clients")
@@ -24,19 +24,19 @@ public class ClientRestController {
         this.clientService = clientService;
     }
 
-    @GetMapping(value = "/", produces = "application/json")
+    @GetMapping(value = "", produces = "application/json")
     public String clientsList() {
         List<Client> clients = clientService.findAll();
         return clients.stream().map(Client::toString).collect(Collectors.joining(", "));
     }
 
-    @PostMapping("/create")
-    public RedirectView newClient(@ModelAttribute ClientCreatePayload client) {
+    @PostMapping("")
+    public HttpStatus createClient(@ModelAttribute ClientCreatePayload client) {
         if (client == null || client.name() == null || client.address() == null || client.phone() == null) {
             throw new IllegalArgumentException("Invalid client data");
         }
         var clientDto = new ClientDto(null, client.name(), client.address(), client.phone());
         clientService.save(clientDto.toClient());
-        return new RedirectView("/clients");
+        return HttpStatus.CREATED;
     }
 }
--- hw14-spring-boot/src/main/java/com/hwspringboot/controllers/ClientUIController.java
+++ hw14-spring-boot/src/main/java/com/hwspringboot/controllers/ClientUIController.java
@@ -1,5 +1,7 @@
 package com.hwspringboot.controllers;
 
+import com.hwspringboot.dto.ClientCreatePayload;
+import com.hwspringboot.dto.ClientDto;
 import com.hwspringboot.model.Client;
 import com.hwspringboot.services.ClientService;
 import java.util.List;
@@ -7,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class ClientUIController {
@@ -27,5 +32,15 @@ public class ClientUIController {
         List<Client> clients = clientService.findAll();
         model.addAttribute("clients", clients);
         return "clients";
+    }
+
+    @PostMapping("/clients")
+    public RedirectView createClient(@ModelAttribute ClientCreatePayload client) {
+        if (client == null || client.name() == null || client.address() == null || client.phone() == null) {
+            throw new IllegalArgumentException("Invalid client data");
+        }
+        var clientDto = new ClientDto(null, client.name(), client.address(), client.phone());
+        clientService.save(clientDto.toClient());
+        return new RedirectView("/clients");
     }
 }
